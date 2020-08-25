1bae3fc47370f7825c35d6488903232904cf2be2
Artem-eduard <artemshykurin98@gmail.com>
2020-08-25 21:59:37 +0900

    second

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,34 @@
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
-let myImage = document.querySelector("image");
+let myImage =document.querySelector("img");
 
-function setUserName() {
-    let myName = prompt("please input your name");
+if (!localStorage.getItem('name')){
+    setUserName();
+}
+else{
+    let myName =localStorage.getItem('name');
+    myHeading.textContent = "Mozilla is cool, " + myName;
+}
 
-    if (!myName) {
+function setUserName(){
+    let myName = prompt("Please input your name");
+    if (!myName){
         setUserName();
-    } else{
+    }else {
+        myHeading.textContent = "Mozilla is cool, " + myName;
         localStorage.setItem('name', myName);
-        myHeading.innerHTML = "Mozilla is cool, " + myName;
     }
 
 }
-if (!localStorage.getItem('name')){
+myButton.onclick = function(){
     setUserName();
-} else{
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedname;
 }
-myButton.onclick=function(){
-    setUserName();
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
 }
